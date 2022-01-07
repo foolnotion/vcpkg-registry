6944e489554d07d46eea8e59d3fea1319d2d9d0f
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-07 12:24:55 +0100

    fix vstat

--- ports/vstat/portfile.cmake
+++ ports/vstat/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO heal-research/vstat
-    REF 3b5b2db297721be5fb5d7613d748e0fa9d2de2f9
-    SHA512 0
+    REF 965a62ae648c48ace71408fcb75029b814104674
+    SHA512 bd2546088e52d576611709c75c3332a5875764e51f6759f3ff48e937e688e015bab5bf1ee07fb8ff7c566e6560ff998b2ba406bdf14b152ad815d3f536bef6c6
     HEAD_REF main
 )
 
@@ -20,8 +20,13 @@ vcpkg_configure_cmake(
         -DBUILD_EXAMPLES=OFF
 )
 vcpkg_cmake_install()
-    vcpkg_cmake_config_fixup(PACKAGE_NAME vstat CONFIG_PATH lib/cmake/vstat DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME vstat CONFIG_PATH lib/cmake/vstat DO_NOT_DELETE_PARENT_CONFIG_PATH)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share"
                     "${CURRENT_PACKAGES_DIR}/lib")
+
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
