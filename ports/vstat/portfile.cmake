87e81cf5289fb81f558323b1b5e684c8bd170dcb
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-07 12:01:54 +0100

    [vstat] new port

---
+++ ports/vstat/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO heal-research/vstat
+    REF 3b5b2db297721be5fb5d7613d748e0fa9d2de2f9
+    SHA512 0
+    HEAD_REF main
+)
+
+include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake/vcpkg_cmake_build.cmake")
+include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake/vcpkg_cmake_install.cmake")
+include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.cmake")
+
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_configure_cmake(
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
+  OPTIONS
+        -DBUILD_TESTING=OFF
+        -DBUILD_EXAMPLES=OFF
+)
+vcpkg_cmake_install()
+    vcpkg_cmake_config_fixup(PACKAGE_NAME vstat CONFIG_PATH lib/cmake/vstat DO_NOT_DELETE_PARENT_CONFIG_PATH)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share"
+                    "${CURRENT_PACKAGES_DIR}/lib")
