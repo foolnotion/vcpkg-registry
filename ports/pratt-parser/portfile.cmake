afa8702a9075338b4bac1afc54b8fed887276586
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-07-04 10:07:56 +0300

    pratt-parser: refresh to latest upstream, modernize portfile
    Not currently a dependency of operon/pyoperon, but it's been sitting
    at a ~2022-era pin using the old vcpkg_configure_cmake API. Bumps to
    foolnotion/pratt-parser-calculator@2e0b136 and switches to the modern
    vcpkg_cmake_configure + declared vcpkg-cmake/vcpkg-cmake-config/
    fast-float dependencies, matching the style used by the other ports
    in this registry (e.g. infix-parser).

--- ports/pratt-parser/portfile.cmake
+++ ports/pratt-parser/portfile.cmake
@@ -1,30 +1,23 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO foolnotion/pratt-parser-calculator 
-    REF 7a73a0cc8005fadeeff34040d247766dd77b18b1 
-    SHA512 3b9625606f2e28d29df609e9f4d018fd2fd6572024ea70f826c8e4cbc6619be5e546a2d362eb38ff7ad529d172da3f3e1c522488f33e74774708a46db5ae8148
+    REPO foolnotion/pratt-parser-calculator
+    REF 2e0b13615c6ff1fb6381c0ac87796932b326bc89
+    SHA512 bdb910209c1936897769f00ca12c3f054068360bb811238e117f862519ea41632688c66a8c78e7763f145537a489e9df00d37728ec75379a2e4b1078106750be
     HEAD_REF main
 )
 
-include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake/vcpkg_cmake_build.cmake")
-include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake/vcpkg_cmake_install.cmake")
-include("${VCPKG_ROOT_DIR}/ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.cmake")
+set(VCPKG_BUILD_TYPE release) # header-only port
 
-set(VCPKG_BUILD_TYPE release)
-
-vcpkg_configure_cmake(
-  SOURCE_PATH "${SOURCE_PATH}"
-  PREFER_NINJA
-  OPTIONS
-        -DBUILD_TESTING=OFF
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
         -DBUILD_EXAMPLES=OFF
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME pratt-parser CONFIG_PATH lib/cmake/pratt-parser DO_NOT_DELETE_PARENT_CONFIG_PATH)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
-                    "${CURRENT_PACKAGES_DIR}/debug/share"
-                    "${CURRENT_PACKAGES_DIR}/lib")
+                    "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
